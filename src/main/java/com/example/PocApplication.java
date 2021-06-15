f71b875e207435f4931c0f7df075206e8a220e11
Kenny Ho <kenny.kc.ho@gtomato.com>
2021-06-15 16:07:09 +0800

    feat (api): Add User Api for testing purpose

--- src/main/java/com/example/PocApplication.java
+++ src/main/java/com/example/PocApplication.java
@@ -2,9 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PocApplication {
+public class PocApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
---
+++ src/main/java/com/example/api/UserApi.java
@@ -0,0 +1,15 @@
+package com.example.api;
+
+import com.example.model.User;
+import org.springframework.http.ResponseEntity;
+
+public class UserApi implements UsersApiDelegate {
+
+    @Override
+    public ResponseEntity<User> getUsers(Long userId) {
+        var user = new User();
+        user.setId(1L);
+        user.setName("Kenny Ho");
+        return ResponseEntity.ok(user);
+    }
+}
