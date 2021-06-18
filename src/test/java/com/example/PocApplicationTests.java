9c377954b231541a99b045481876a0cfbe724d74
Kenny Ho <kenny.kc.ho@gtomato.com>
2021-06-18 17:56:41 +0800

    [WIP] feat: Add Unit test

--- src/test/java/com/example/PocApplicationTests.java
+++ src/test/java/com/example/PocApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import com.example.service.GeneralService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PocApplicationTests {
 
+    @Autowired
+    private GeneralService generalService;
+
     @Test
-    void contextLoads() {
+    void validSchema() throws Exception {
+        generalService.validateContext();
     }
 
 }
